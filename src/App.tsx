2490a95f26ff7cc9a4275d568e723ccbe8f53c93
eric valcik <eric.valcik@gmail.com>
2024-12-30 00:04:45 +0100

    something :D

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
 import "./App.css";
 
+const days = 9037;
+const maxWeeks = 4174;
+
+const myWeeks = Math.floor(days / 7);
+const rows = Math.ceil(maxWeeks / 52);
+
 const App = () => {
-  return <div>Hi from react</div>;
+  return Array.from({ length: rows }).map((_, i) => {
+    const weeks = i === 80 ? 14 : 52;
+    return (
+      <div className="row">
+        {Array.from({ length: weeks }).map((_, j) => {
+          const currentWeek = i * 52 + j;
+          return (
+            <div
+              className={`week ${currentWeek < myWeeks ? "filled" : ""} ${j % 4 === 3 ? "padded-right" : ""}`}
+            ></div>
+          );
+        })}
+      </div>
+    );
+  });
 };
 
 export default App;
