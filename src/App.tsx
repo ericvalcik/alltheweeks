696aa8b21deeb792d66ff17c9501088df42e0584
eric valcik <eric.valcik@gmail.com>
2024-12-29 23:36:11 +0100

    animation workingg

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,7 @@
-import { useEffect, useState } from "react";
 import "./App.css";
 
-import { Stage, Sprite, useApp, useTick } from "@pixi/react";
-
 const App = () => {
-  const [frames, setFrames] = useState([]);
-  const [rot, setRot] = useState(0);
-  const app = useApp();
-
-  useTick((delta) => setRot((r) => r + 0.01 * delta));
-
-  // load
-  useEffect(() => {
-    app.loader
-      .add(
-        "https://pixijs.io/examples/examples/assets/spritesheet/fighter.json",
-      )
-      .load((_, resource) => {
-        setFrames(
-          Object.keys(resource[spritesheet].data.frames).map((frame) =>
-            PIXI.Texture.from(frame),
-          ),
-        );
-      });
-  }, []);
-
-  if (frames.length === 0) {
-    return null;
-  }
-
-  return (
-    <Stage>
-      <Container rotation={rot} x={width / 2} y={height / 2}>
-        <AnimatedSprite
-          animationSpeed={0.5}
-          isPlaying={true}
-          textures={frames}
-          anchor={0.5}
-        />
-      </Container>
-    </Stage>
-  );
+  return <div>Hi from react</div>;
 };
 
 export default App;
---
+++ src/main-pixi.ts
@@ -0,0 +1,43 @@
+import { Application, Assets, AnimatedSprite } from "pixi.js";
+
+(async () => {
+  // Create a new application
+  const app = new Application();
+
+  // Initialize the application
+  await app.init({ background: "#1099bb", width: 150, height: 150 });
+
+  // Append the application canvas to the document body
+  document.getElementById("pixi-container")!.appendChild(app.canvas);
+
+  // Load ador
+  const sheet = await Assets.load("/assets/ador.json");
+
+  const adorIdle = new AnimatedSprite(sheet.animations["idle"]);
+  adorIdle.texture.source.scaleMode = "nearest";
+  adorIdle.animationSpeed = 0.07;
+  adorIdle.scale.set(4);
+  adorIdle.anchor.set(0.5);
+  adorIdle.position.set(app.screen.width / 2, app.screen.height / 2);
+
+  const adorRun = new AnimatedSprite(sheet.animations["run"]);
+  adorRun.texture.source.scaleMode = "nearest";
+  adorRun.animationSpeed = 0.12;
+  adorRun.scale.set(4);
+  adorRun.anchor.set(0.5);
+  adorRun.position.set(app.screen.width / 2, app.screen.height / 2);
+
+  // Add the bunny to the stage
+  app.stage.addChild(adorIdle);
+
+  // Play the animation
+  adorIdle.play();
+
+  // Listen for animate update
+  app.ticker.add((time) => {
+    // Just for fun, let's rotate mr rabbit a little.
+    // * Delta is 1 if running at 100% performance *
+    // * Creates frame-independent transformation *
+    // ador.rotation += 0.1 * time.deltaTime;
+  });
+})();
